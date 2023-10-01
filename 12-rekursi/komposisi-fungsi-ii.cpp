9685c0b4f57d34e5eb8eb8d8a93c2ef9dea9ae99
adityaazizi <adityaauliaalazizi@gmail.com>
2023-10-01 20:57:14 +0700

    add course 12

---
+++ 12-rekursi/komposisi-fungsi-ii.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int A, int B, int x) {
+    return abs(A * x + B);
+}
+
+int applyingKtimes(int A, int B, int K, int x) {
+    for (int i = 0; i < K; i++) {
+        x = f(A, B, x);
+    }
+    return x;
+}
+
+int main() {
+    int A, B, K, x;
+    cin >> A >> B >> K >> x;
+
+    cout << applyingKtimes(A, B, K, x) << endl;
+    return 0;
+}
