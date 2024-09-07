5947bcc334aacf162302f45f80e9f21f53ec671d
adityaazizi <azizi.business@gmail.com>
2024-09-07 10:29:57 +0700

    add course 2

---
+++ 02-variabel-dan-tipe-data/03-perkenalan-tipe-data-string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tanggal = 15;
+    int tahun = 2023;
+
+    // jangan lupa bahwa string perlu diapit dengan kutip dua
+    string bulan = "Februari";
+
+    // cetak kata sandi
+    cout << tahun + 10 << "-" << bulan << "-" << tanggal + 7 << endl;
+}
