396bbfbd42d454d14b2e5bdd71b3c13ea22135e4
adityaazizi <azizi.business@gmail.com>
2024-09-07 10:30:12 +0700

    course 3

---
+++ 03-perulangan/03-membeli-kandang-II.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+    int nomor_baris = 1;
+
+    while (nomor_baris <= 3) {
+        luas_kandang += 7;
+        cout << nomor_baris << ": " << luas_kandang << endl;
+        nomor_baris += 1;
+    }
+}
