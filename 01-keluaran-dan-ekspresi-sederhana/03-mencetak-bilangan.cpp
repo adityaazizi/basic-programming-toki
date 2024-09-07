12c9953e067d852876511270188af50ce4fed649
adityaazizi <azizi.business@gmail.com>
2024-09-07 10:29:44 +0700

    add course 1

---
+++ 01-keluaran-dan-ekspresi-sederhana/03-mencetak-bilangan.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Pak Dengklek memiliki " << 75 << " ekor bebek." << endl;
+}
---
+++ 01-keluaran-dan-ekspresi-sederhana/04-menjumlahkan-bilangan.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Pak Dengklek memiliki " << 738 + 519 << " ekor bebek." << endl;
+}
