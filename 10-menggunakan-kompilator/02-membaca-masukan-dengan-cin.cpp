b7211c1305a21b2625d7eb11b023a43f2c24d2d8
adityaazizi <azizi.business@gmail.com>
2024-09-08 14:59:25 +0700

    add course 10

---
+++ 10-menggunakan-kompilator/02-membaca-masukan-dengan-cin.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long faktorial(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return faktorial(n - 1) * n;
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << faktorial(n) + faktorial(m) << endl;
+}
---
+++ 10-menggunakan-kompilator/03-membaca-mencetak-dari-berkas.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int banyaknya_vokal = 0;
+    for (char c : s) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            banyaknya_vokal++;
+        }
+    }
+
+    cout << banyaknya_vokal << endl;
+}
